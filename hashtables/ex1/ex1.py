e7b0966158169e078f0303591c64ce7551ef8a41
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-06-04 16:40:43 -0700

    ex1 is almost working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,30 @@
+# weights_3 = [4, 6, 10, 15, 16]
+# weights_2 = [4, 4]
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    times = 0
+    for i in range(0, length):
+        times += 1
+        print('it ran: ', times)
+        weight_needed =  limit - weights[i]
+        if weight_needed in cache:
+            print('in cache')
+            weight1 = cache[weight_needed]
+            weight2 = weights[i]
+            if weight1 >= weight2:
+                print('first if')
+                return [i, weight1]
+            elif weight2 > weight1:
+                return [weight1, i]
 
+        else:
+            cache[weights[i]] = i
     return None
+
+# print(get_indices_of_item_weights(weights_3, 5, 21))
+# print(get_indices_of_item_weights(weights_2, 2, 8))
